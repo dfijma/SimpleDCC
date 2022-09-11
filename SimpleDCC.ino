b82d366e43236289a05b8cd5f8d5e13792e65467
Duco Fijma <duco@fijma.net>
2022-09-11 14:17:21 +0200

    power via motor shield

--- SimpleDCC.ino
+++ SimpleDCC.ino
@@ -3,8 +3,19 @@ const int DCC_SIGNAL_PIN_MAIN = 5;
 
 int potValue = 0;  // variable to store the value coming from the sensor
 
+const int DIRECTION_MOTOR_CHANNEL_PIN_A = 12;
+const int SPEED_MOTOR_CHANNEL_PIN_A = 3;
+const int BRAKE_MOTOR_CHANNEL_PIN_A = 9;
+
 void setup() {
   pinMode(DCC_SIGNAL_PIN_MAIN, OUTPUT);
+
+  disableDirectionOutput();
+
+  pinMode(SPEED_MOTOR_CHANNEL_PIN_A, OUTPUT);  
+  pinMode(BRAKE_MOTOR_CHANNEL_PIN_A, OUTPUT);
+
+  powerOn();
 }
 
 void loop() {
@@ -13,4 +24,14 @@ void loop() {
   delay(potValue);
   digitalWrite(DCC_SIGNAL_PIN_MAIN, LOW);
   delay(potValue);
+}
+
+void powerOn() {
+  digitalWrite(SPEED_MOTOR_CHANNEL_PIN_A, HIGH);
+  digitalWrite(BRAKE_MOTOR_CHANNEL_PIN_A, LOW); 
+}
+
+void disableDirectionOutput() {
+  pinMode(DIRECTION_MOTOR_CHANNEL_PIN_A, INPUT);
+  digitalWrite(DIRECTION_MOTOR_CHANNEL_PIN_A, LOW);
 }
